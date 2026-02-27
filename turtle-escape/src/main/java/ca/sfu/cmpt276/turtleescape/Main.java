902576fd424ec9204ee134df1d23a528d1ce7d46
Darragh Donnelly <darraghsfu@gmail.com>
2026-02-26 17:22:06 -0800

    finished maven pom file

--- turtle-escape/src/main/java/ca/sfu/cmpt276/turtleescape/Main.java
+++ turtle-escape/src/main/java/ca/sfu/cmpt276/turtleescape/Main.java
@@ -3,7 +3,7 @@ package ca.sfu.cmpt276.turtleescape;
 /**
  * Hello world!
  */
-public class App {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
